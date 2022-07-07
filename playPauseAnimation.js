9766c17d32e2ead3bf243aef55b5a1700e261fdd
hbalickgoodman <hbalickgoodman@gmail.com>
2022-07-07 15:49:40 -0400

    Added play/pause animation functionality

--- playPauseAnimation.js
+++ playPauseAnimation.js
@@ -1,4 +1,5 @@
 const controllers = document.getElementsByClassName("play-pause-icon");
+const floating = document.getElementsByClassName("floating");
 function stopstartanimate() {
 
     for (const icon of controllers) {
@@ -6,11 +7,17 @@ function stopstartanimate() {
             icon.classList.add("play");
             icon.classList.remove("pause");
             makePlay = false;
+            for (const image of floating) {
+                image.classList.add("stop-animation");
+            }
         } else {
             console.log("run");
             icon.classList.add("pause");
             icon.classList.remove("play");
             makePlay = true;
+            for (const image of floating) {
+                image.classList.remove("stop-animation");
+            }
         }
 
     }
