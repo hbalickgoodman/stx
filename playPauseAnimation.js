6b2694d8d42cca453e655db27732855ae5f1508f
hbalickgoodman <hbalickgoodman@gmail.com>
2022-07-13 10:29:39 -0400

    Added tabbing/enter compatibility to forms

--- playPauseAnimation.js
+++ playPauseAnimation.js
@@ -36,9 +36,31 @@ function ShiftBack() {
     }
 }
 
-$(document).keyup(function (event) {
-    if (event.which === 13) {
-        $("[input[type='radio']:checked+label:after").css("background-color", "yellow");
-        console.log('Enter is pressed!');
-    }
+$(document).ready(function () {
+
+    //detect if a label within our container is pressed
+    $('.bottomForm label').keypress(function (e) {
+
+        //set variable for the key, e is the event object being passed
+        var key = e.which;
+
+        //check if the variable is equal to 13 (the return key value)
+        if (key == 13) {
+
+            //set variable for this radio button attribute
+            var forAttribute = $(this).attr("for");
+
+            //if this is already checked, uncheck it
+            if ($('#' + forAttribute).is(':checked')) {
+                $('#' + forAttribute).prop("checked", false);
+            }
+            //if this is not checked, check it
+            else {
+                $('#' + forAttribute).prop("checked", true);
+            }
+
+        }
+
+    });
+
 });
